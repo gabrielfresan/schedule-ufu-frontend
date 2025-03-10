c6bf06406997f7c346cf384625108fb5e2fb4232
Gabriel Machado <gabriel_machado@ufu.br>
2025-03-10 20:06:10 -0300

    Criando casca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeadersComponent } from './components/headers/headers.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    HeadersComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
