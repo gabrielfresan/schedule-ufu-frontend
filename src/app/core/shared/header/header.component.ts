f12f2a091d19bd9c8ce193275961c8d483feb47b
Gabriel Machado <gabriel_machado@ufu.br>
2025-04-03 22:22:54 -0300

    fixing css

--- src/app/core/shared/header/header.component.ts
+++ src/app/core/shared/header/header.component.ts
@@ -8,5 +8,22 @@ import { Component, Input } from '@angular/core';
 })
 export class HeaderComponent {
   @Input() user: string = 'Fulano de Tal';
+  randomColor: string = this.getRandomColor();
+
+  getRandomColor(): string {
+    const colors = [
+      'bg-red-500',    // Vermelho forte
+      'bg-blue-500',   // Azul forte
+      'bg-green-500',  // Verde forte
+      'bg-yellow-600', // Amarelo escuro
+      'bg-orange-500', // Laranja forte
+      'bg-purple-500', // Roxo forte
+      'bg-pink-500',   // Rosa forte
+      'bg-teal-500',   // Verde-azulado forte
+      'bg-indigo-500', // Índigo forte
+      'bg-gray-800'    // Cinza escuro
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
 
 }
