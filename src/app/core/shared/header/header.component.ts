f6744f39bf3e59244b9daf7b1a6ba72044cb88fe
Gabriel Machado <gabriel_machado@ufu.br>
2025-04-03 23:52:31 -0300

    fix css and tyind to enhance img

--- src/app/core/shared/header/header.component.ts
+++ src/app/core/shared/header/header.component.ts
@@ -12,18 +12,11 @@ export class HeaderComponent {
 
   getRandomColor(): string {
     const colors = [
-      'bg-red-500',    // Vermelho forte
-      'bg-blue-500',   // Azul forte
-      'bg-green-500',  // Verde forte
-      'bg-yellow-600', // Amarelo escuro
-      'bg-orange-500', // Laranja forte
-      'bg-purple-500', // Roxo forte
-      'bg-pink-500',   // Rosa forte
-      'bg-teal-500',   // Verde-azulado forte
-      'bg-indigo-500', // Índigo forte
-      'bg-gray-800'    // Cinza escuro
+      'bg-blue-50',
+      'bg-red-50',
+      'bg-orange-50'
     ];
-    return colors[Math.floor(Math.random() * colors.length)];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
   }
-
 }
