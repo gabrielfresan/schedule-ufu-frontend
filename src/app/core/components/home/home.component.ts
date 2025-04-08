59c74553c9d972b569c5d75f57f2e059396c3b2e
Gabriel Rezende Machado <47564540+grm1003@users.noreply.github.com>
2025-04-07 21:59:33 -0300

    Filtro ok

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Reserva } from '../../models/reservaModel';
 import { CommonModule } from '@angular/common';
 import flatpickr from 'flatpickr';
@@ -7,63 +7,98 @@ import flatpickr from 'flatpickr';
   selector: 'app-home',
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  ngAfterViewInit(): void {
-    flatpickr('#simples-input', {
-    });
-  }
-    
+export class HomeComponent implements AfterViewInit {
+  // Select configuration
+  @ViewChild('dropdownList') dropdownList!: ElementRef;
+  isDropdownOpen: boolean = false;
+
+  // Todas as reservas da tabela
   reserva: Reserva[] = [
     {
       horario: '08:00 - 09:00',
       nome: 'G1',
       resposavel: 'João Silva',
       curso: 'Engenharia de Software',
-      disponibilidade: 'Disponível'
+      disponibilidade: 'Disponível',
+      campus: 'Santa Mônica'
     },
     {
       horario: '16:30 - 17:30',
       nome: 'G5',
       resposavel: 'Maria Oliveira',
       curso: 'Ciência da Computação',
-      disponibilidade: 'Indisponível'
+      disponibilidade: 'Indisponível',
+      campus: 'Santa Mônica'
     },
     {
       horario: '12:00 - 14:00',
       nome: 'Campo Futebol',
       resposavel: 'Carlos Souza',
       curso: 'Sistemas de Informação',
-      disponibilidade: 'Disponível'
+      disponibilidade: 'Disponível',
+      campus: 'FAEFI'
     },
     {
       horario: '12:00 - 14:00',
       nome: 'G1',
       resposavel: 'Ana Costa',
       curso: 'Engenharia Elétrica',
-      disponibilidade: 'Indisponível'
+      disponibilidade: 'Indisponível',
+      campus: 'Santa Mônica'
     },
     {
       horario: '08:00 - 09:00',
       nome: 'G2',
       resposavel: 'Gabriel Machado',
       curso: 'Sistemas de Informação',
-      disponibilidade: 'Indisponível'
+      disponibilidade: 'Indisponível',
+      campus: 'Santa Mônica'
     },
   ];
 
+    // Variáveis para o filtro e lista a ser mostrada
+  selectedCampus: string = '';
+  selectedLabel: string = "Selecione o Campus para filtrar";
+  filteredReserva: Reserva[] = [];
 
+  // Configurações iniciais do Flatpickr e filtro
+  ngAfterViewInit(): void {
+    flatpickr('#simples-input', {});
+    this.filteredReserva = this.reserva; 
+  }
+
+  //lógica do dropdown
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    if (this.isDropdownOpen) {
+      this.dropdownList.nativeElement.style.display = 'absolute';
+    } else {
+      this.dropdownList.nativeElement.style.display = 'none';
+    }
+  }
 
   // Select (Dropdown) configuration
   dropdownOptions = [
-    { id: 'criar-reserva', value: 'criar-reserva', label: 'Criar Reserva' },
-    { id: 'editar-reserva', value: 'editar-reserva', label: 'Editar Reserva' },
-    { id: 'cancelar-reserva', value: 'cancelar-reserva', label: 'Cancelar Reserva' },
+    { id: 'santa-monica', value: 'Santa Mônica', label: 'Santa Mônica' },
+    { id: 'faefi', value: 'FAEFI', label: 'FAEFI' },
+    { id: 'todos', value: '', label: 'Todos', default: true },
   ];
 
+  //Exibição da lista
+  onDropdownChange(selectedValue: string): void {
+    this.selectedCampus = selectedValue;
+    this.selectedLabel = selectedValue;
+    this.filterTable();
+    this.toggleDropdown();
+  }
 
-  onDropdownChange(event: String): void {
-    console.log('Campus selecionado:', event);
+  filterTable(): void {
+    if (this.selectedCampus) {
+      this.filteredReserva = this.reserva.filter(row => row.campus == this.selectedCampus);
+    } else {
+      this.filteredReserva = this.reserva;
+    }
   }
 }
--- src/app/core/models/reservaModel.ts
+++ src/app/core/models/reservaModel.ts
@@ -1,6 +1,7 @@
 export interface Reserva {
     horario: string;
     nome: string;
+    campus: string;
     resposavel: string;
     curso: string;
     disponibilidade: string;
