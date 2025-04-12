ad4333977b15ff5170832a864bc64547758b627f
Gabriel Rezende Machado <47564540+grm1003@users.noreply.github.com>
2025-04-12 18:25:01 -0300

    Comecando pagina minhas reservas

--- src/app/core/pages/userschdeules/userschdeules.component.ts
+++ src/app/core/pages/userschdeules/userschdeules.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableComponent } from "../../components/table/table.component";
+import { SelectFilterComponent } from "../../components/select-filter/select-filter.component";
+import { option } from '../../models/Option';
+import { DatapickerComponent } from "../../components/datapicker/datapicker.component";
+import { Reserva } from '../../models/Reserva';
 
 @Component({
   selector: 'app-userschdeules',
-  imports: [],
+  imports: [CommonModule, TableComponent, SelectFilterComponent, DatapickerComponent],
   templateUrl: './userschdeules.component.html',
   styleUrl: './userschdeules.component.css'
 })
 export class UserschdeulesComponent {
+    // Todas as reservas da tabela
+    reserva: Reserva[] = [
+      {
+        horario: '08:00 - 09:00',
+        nome: 'G2',
+        resposavel: 'Gabriel Machado',
+        curso: 'Sistemas de Informação',
+        disponibilidade: 'Indisponível',
+        campus: 'Santa Mônica'
+      },
+      {
+        horario: '10:00 - 12:00',
+        nome: 'G1 - Tenis de Mesa',
+        resposavel: 'Gabriel Machado',
+        curso: 'Sistemas de Informação',
+        disponibilidade: 'Indisponível',
+        campus: 'FAEFI'
+      }
+    ];
+  
+    // Variáveis para o filtro e lista a ser mostrada
+    selectedCampus: string = '';
+    selectedLabel: string = "Selecione o Campus para filtrar";
+    filteredReserva: Reserva[] = [];
+  
+    filterTable(): void {
+      if (this.selectedCampus) {
+        this.filteredReserva = this.reserva.filter(row => row.campus == this.selectedCampus);
+      } else {
+        this.filteredReserva = this.reserva;
+      }
+    }
+  
+  
+    // Configurações iniciais do Flatpickr e filtro
+    @ViewChild(DatapickerComponent) datapickerComponent!: DatapickerComponent;
+    ngAfterViewInit(): void {
+      this.filteredReserva = this.reserva;
+    }
+  
+    // Select Options(Dropdown)
+    dropdownOptions : Array<option> = [
+      { id: 'todos', value: '', label: 'Todos'},
+      { id: 'santa-monica', value: 'Santa Mônica', label: 'Santa Mônica' },
+      { id: 'faefi', value: 'FAEFI', label: 'FAEFI' },
+    ];
+  
+    //Exibição da lista
+    onOptionSelected(option : option) {
+      this.selectedCampus = option.value;
+      this.selectedLabel = option.label;
+      this.filterTable();
+    }
 
 }
