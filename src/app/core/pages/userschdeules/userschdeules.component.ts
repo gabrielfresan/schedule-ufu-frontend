3185662b003f87de0b0af02eda83a19645bda3cd
Giovanna <giovannaevangelista78@gmail.com>
2025-04-12 19:01:19 -0300

    ajuste na tabela para renderizacao imediata assim que pagina aberta

--- src/app/core/pages/userschdeules/userschdeules.component.ts
+++ src/app/core/pages/userschdeules/userschdeules.component.ts
@@ -1,70 +1,62 @@
-import { Component, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TableComponent } from "../../components/table/table.component";
-import { SelectFilterComponent } from "../../components/select-filter/select-filter.component";
-import { option } from '../../models/Option';
-import { DatapickerComponent } from "../../components/datapicker/datapicker.component";
+import { Component, OnInit } from '@angular/core';
 import { Reserva } from '../../models/Reserva';
+import { TableComponent } from '../../components/table/table.component';
+import { DatapickerComponent } from '../../components/datapicker/datapicker.component';
+import { SelectFilterComponent } from '../../components/select-filter/select-filter.component';
+import { CommonModule } from '@angular/common';
+import { option } from '../../models/Option';
 
 @Component({
   selector: 'app-userschdeules',
-  imports: [CommonModule, TableComponent, SelectFilterComponent, DatapickerComponent],
   templateUrl: './userschdeules.component.html',
-  styleUrl: './userschdeules.component.css'
+  styleUrl: './userschdeules.component.css',
+  imports: [CommonModule, TableComponent, DatapickerComponent, SelectFilterComponent]
 })
-export class UserschdeulesComponent {
-    // Todas as reservas da tabela
-    reserva: Reserva[] = [
-      {
-        horario: '08:00 - 09:00',
-        nome: 'G2',
-        resposavel: 'Gabriel Machado',
-        curso: 'Sistemas de Informação',
-        disponibilidade: 'Indisponível',
-        campus: 'Santa Mônica'
-      },
-      {
-        horario: '10:00 - 12:00',
-        nome: 'G1 - Tenis de Mesa',
-        resposavel: 'Gabriel Machado',
-        curso: 'Sistemas de Informação',
-        disponibilidade: 'Indisponível',
-        campus: 'FAEFI'
-      }
-    ];
-  
-    // Variáveis para o filtro e lista a ser mostrada
-    selectedCampus: string = '';
-    selectedLabel: string = "Selecione o Campus para filtrar";
-    filteredReserva: Reserva[] = [];
-  
-    filterTable(): void {
-      if (this.selectedCampus) {
-        this.filteredReserva = this.reserva.filter(row => row.campus == this.selectedCampus);
-      } else {
-        this.filteredReserva = this.reserva;
-      }
+export class UserschdeulesComponent implements OnInit {
+  reserva: Reserva[] = [
+    {
+      horario: '08:00 - 09:00',
+      nome: 'G2',
+      resposavel: 'Gabriel Machado',
+      curso: 'Sistemas de Informação',
+      disponibilidade: 'Indisponível',
+      campus: 'Santa Mônica'
+    },
+    {
+      horario: '10:00 - 12:00',
+      nome: 'G1 - Tenis de Mesa',
+      resposavel: 'Gabriel Machado',
+      curso: 'Sistemas de Informação',
+      disponibilidade: 'Indisponível',
+      campus: 'FAEFI'
     }
-  
-  
-    // Configurações iniciais do Flatpickr e filtro
-    @ViewChild(DatapickerComponent) datapickerComponent!: DatapickerComponent;
-    ngAfterViewInit(): void {
+  ];
+
+  filteredReserva: Reserva[] = [];
+  selectedCampus: string = '';
+  selectedLabel: string = "Selecione o Campus para filtrar";
+
+  dropdownOptions: Array<option> = [
+    { id: 'todos', value: '', label: 'Todos' },
+    { id: 'santa-monica', value: 'Santa Mônica', label: 'Santa Mônica' },
+    { id: 'faefi', value: 'FAEFI', label: 'FAEFI' }
+  ];
+
+  ngOnInit(): void {
+    this.filteredReserva = this.reserva;
+  }
+
+  onOptionSelected(option: { value: string; label: string }): void {
+    this.selectedCampus = option.value;
+    this.selectedLabel = option.label;
+    this.filterTable();
+  }
+
+  filterTable(): void {
+    if (this.selectedCampus) {
+      this.filteredReserva = this.reserva.filter(row => row.campus === this.selectedCampus);
+    } else {
       this.filteredReserva = this.reserva;
     }
-  
-    // Select Options(Dropdown)
-    dropdownOptions : Array<option> = [
-      { id: 'todos', value: '', label: 'Todos'},
-      { id: 'santa-monica', value: 'Santa Mônica', label: 'Santa Mônica' },
-      { id: 'faefi', value: 'FAEFI', label: 'FAEFI' },
-    ];
-  
-    //Exibição da lista
-    onOptionSelected(option : option) {
-      this.selectedCampus = option.value;
-      this.selectedLabel = option.label;
-      this.filterTable();
-    }
-
+  }
 }
